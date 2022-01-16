62a25518beded92225ba6c96a2219f0b055a3ab1
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-16 13:25:33 +0000

    - Unwrap children if array

--- jsx/factory.ts
+++ jsx/factory.ts
@@ -7,13 +7,20 @@ export interface ElementAttributes {
 export function factory(
   tag: (props: JSX.ComponentProps) => string | JSX.Element,
   attributes: ElementAttributes,
-  ...children: string[] | JSX.Element[]
+  ...children: JSX.Element[] | JSX.Element[][]
 ) {
   if (typeof tag === "string") {
-    return { tag, ...attributes, children };
+    return { tag, ...attributes, children: unwrap(children) };
   }
   if (typeof tag === "function") {
-    return tag({ ...attributes, children });
+    return tag({ ...attributes, children: unwrap(children) });
   }
   return {};
+}
+
+function unwrap(children: JSX.Element[] | JSX.Element[][]): JSX.Element[] {
+  if (Array.isArray(children[0])) {
+    return children[0];
+  }
+  return <JSX.Element[]> children;
 }
