f41d5cc8dad3783cdc8a5ca1d04f79921fce31a2
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-23 21:43:40 +0000

    Upgrade deno versions

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { Get } from "https://deno.land/x/cargo@0.1.15/http/mod.ts";
+export { Get } from "https://deno.land/x/cargo@0.1.16/http/mod.ts";
 export {
   extension,
   log,
   name,
-} from "https://deno.land/x/cargo@0.1.15/utils/mod.ts";
+} from "https://deno.land/x/cargo@0.1.16/utils/mod.ts";
