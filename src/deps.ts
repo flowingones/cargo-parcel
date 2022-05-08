1377b8884d29b3d260b0e6156067abf6562c4e25
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-05-08 12:44:48 +0000

    Raise Cargo Parcel Version to 0.1.33

--- src/deps.ts
+++ src/deps.ts
@@ -1,8 +1,8 @@
 export { dirname } from "https://deno.land/std@0.123.0/path/mod.ts";
 
-export { Get } from "https://deno.land/x/cargo@0.1.32/http/mod.ts";
+export { Get } from "https://deno.land/x/cargo@0.1.33/http/mod.ts";
 export {
   extension,
   log,
   name,
-} from "https://deno.land/x/cargo@0.1.32/utils/mod.ts";
+} from "https://deno.land/x/cargo@0.1.33/utils/mod.ts";
