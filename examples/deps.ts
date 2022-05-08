665993a0b807eaa3e62986500dc2eeae533dfa2d
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-05-08 14:27:47 +0000

    Upgrade Cargo version to 0.1.34

--- examples/deps.ts
+++ examples/deps.ts
@@ -1,9 +1,7 @@
-export { tag } from "https://raw.githubusercontent.com/flowingones/cargo-parcel/dev/src/mod.ts";
+export { tag } from "../src/mod.ts";
 
-export { StaticPage } from "https://raw.githubusercontent.com/flowingones/cargo-parcel/dev/src/http/mod.ts";
+export { StaticPage } from "../src/http/mod.ts";
 
-export {
-  Root,
-} from "https://raw.githubusercontent.com/flowingones/cargo-parcel/dev/src/page/mod.ts";
+export { Root } from "../src/page/mod.ts";
 
-export { bootstrap } from "https://deno.land/x/cargo@0.1.31/mod.ts";
+export { bootstrap } from "https://deno.land/x/cargo@0.1.34/mod.ts";
