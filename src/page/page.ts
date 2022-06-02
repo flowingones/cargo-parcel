af197d2acaaa70b20f9e291ec87355381073d306
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-02 11:07:49 +0000

    Decouple page preparation from route

---
+++ src/page/page.ts
@@ -0,0 +1,42 @@
+import { render, tag, Twind } from "./deps.ts";
+import { title } from "./mod.ts";
+
+interface Page {
+  path: string;
+  title?: string;
+  component: (props: JSX.ElementProps) => JSX.Element;
+  twind?: boolean;
+}
+
+export function page(
+  page: Page,
+  root: (props: JSX.ElementProps) => JSX.Element,
+) {
+  if (page.title) {
+    title(page.title);
+  }
+
+  const content = render(tag(page.component, {}, []));
+  let twind = "";
+  Twind.reset();
+  if (page.twind) {
+    twind = Twind.styleTag(Twind.sheet());
+  }
+  const response = new Response(
+    `<!DOCTYPE html>${render(tag(root, { twind, content }))}`,
+    {
+      headers: {
+        "content-type": "text/html",
+      },
+    },
+  );
+
+  cleanup();
+
+  return response;
+}
+
+export function cleanup() {
+  title("");
+  Twind.reset();
+}
