972708b34c8d4f3c13327736e4ca060e8462d87d
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-03 16:19:17 +0000

    Upgrade to Cargo Parcel Version 0.1.38

--- examples/deps.ts
+++ examples/deps.ts
@@ -1,2 +1,2 @@
-export { tag } from "https://deno.land/x/cargo_parcel@0.1.37/mod.ts";
-export { Title } from "https://deno.land/x/cargo_parcel@0.1.37/page/mod.ts";
+export { tag } from "https://deno.land/x/cargo_parcel@0.1.38/mod.ts";
+export { Title } from "https://deno.land/x/cargo_parcel@0.1.38/page/mod.ts";
--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,4 @@
-// Cargo Parcel – Version 0.1.37
+// Cargo Parcel – Version 0.1.38
 export { tag } from "./tag.ts";
 export {
   type ElementProps,
