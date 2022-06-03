8b7fdf8b191c33f660d081bdd86e68ebc146e53b
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-03 16:21:04 +0000

    Remove future exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,2 @@
 // Cargo Parcel – Version 0.1.38
 export { tag } from "./tag.ts";
-export {
-  type ElementProps,
-  type VComponent,
-  type VElement,
-  type VNode,
-  type VText,
-} from "./node-types.ts";
-export { componentsCache } from "./cache.ts";
-export { cycle, listener } from "./cycle.ts";
