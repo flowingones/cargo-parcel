09c96b20903868aa7f5a1741e7a93d7d3aa8001a
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-09 18:36:49 +0000

    Upgrade to Cargo V 0.1.40

--- examples/deps.server.ts
+++ examples/deps.server.ts
@@ -1,6 +1,6 @@
-export { StaticPage } from "https://deno.land/x/cargo_parcel@0.1.39/http/mod.ts";
-export { page } from "https://deno.land/x/cargo_parcel@0.1.39/page/page.ts";
-export { bootstrap } from "https://deno.land/x/cargo@0.1.39/mod.ts";
-export { Get } from "https://deno.land/x/cargo@0.1.39/http/mod.ts";
+export { StaticPage } from "https://deno.land/x/cargo_parcel@0.1.40/http/mod.ts";
+export { page } from "https://deno.land/x/cargo_parcel@0.1.40/page/page.ts";
+export { bootstrap } from "https://deno.land/x/cargo@0.1.40/mod.ts";
+export { Get } from "https://deno.land/x/cargo@0.1.40/http/mod.ts";
 
 export { tw } from "https://cdn.skypack.dev/twind";
--- src/mod.ts
+++ src/mod.ts
@@ -1,2 +1,2 @@
-// Cargo Parcel – Version 0.1.39
+// Cargo Parcel – Version 0.1.40
 export { tag } from "./tag.ts";
