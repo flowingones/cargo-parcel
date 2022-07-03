80df0eca10a4eebd6cad2fc9e431a45cf6a3ab46
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-07-03 19:25:15 +0000

    refactor: add ast and registry to module exports

--- src/mod.ts
+++ src/mod.ts
@@ -1,2 +1,10 @@
 // Cargo Parcel – Version 0.1.41
 export { tag } from "./tag.ts";
+export { componentsCache } from "./registry.ts";
+export {
+  ast,
+  type VComponent,
+  type VElement,
+  type VNode,
+  type VText,
+} from "./ast.ts";
