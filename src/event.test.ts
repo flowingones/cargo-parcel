39c7277a482c97d8e4639624e409662793bc51c6
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-08-28 14:05:42 +0000

    test: event helper

---
+++ src/event.test.ts
@@ -0,0 +1,16 @@
+import { eventName, isEventName } from "./event.ts";
+import { assertEquals } from "./test_deps.ts";
+
+Deno.test("Event: isEventName", () => {
+  assertEquals(isEventName(""), false);
+  assertEquals(isEventName(""), false);
+  assertEquals(isEventName("on-click is is"), false);
+  assertEquals(isEventName("on-click"), true);
+});
+
+Deno.test("Event: eventName", () => {
+  assertEquals(eventName(""), "");
+  assertEquals(eventName("On-click"), "On-click");
+  assertEquals(eventName("on-click is is"), "click is is");
+  assertEquals(eventName("on-click"), "click");
+});
