074f2af1bd22822f9fd49fb7085b2f025cd08d21
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-08-30 09:30:36 +0000

    refactor: add title, page and footer to mod.ts

--- src/cargo/mod.ts
+++ src/cargo/mod.ts
@@ -1,4 +1,7 @@
 export { mappedPath } from "./path-mapping.ts";
+export { footer } from "./footer.ts";
+export { title } from "./title.ts";
 export { head } from "./head.ts";
 export { body } from "./body.ts";
 export { html } from "./html.ts";
+export { page } from "./page.ts";
