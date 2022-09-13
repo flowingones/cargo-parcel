33549c14414fc21378743a72268f28eab75b9b52
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-09-13 09:54:54 +0000

    feat: esbuild support for deno deploy
    WIP: Use esbuild wasm for deno deploy

--- src/cargo/bundle.ts
+++ src/cargo/bundle.ts
@@ -4,6 +4,8 @@ interface BundleProps {
   islands: Record<string, JSX.Component>;
 }
 
+let esbuildInit = false;
+
 export async function bundle(props: BundleProps) {
   const entryPoints: Record<string, string> = {
     main: new URL("../platform/browser/launch.ts", import.meta.url).href,
@@ -11,6 +13,14 @@ export async function bundle(props: BundleProps) {
 
   for (const island in props.islands) {
     entryPoints[`island-${parse(island).name}`] = `./${island}`;
+  }
+
+  if (Deno.run === undefined && !esbuildInit) {
+    await esbuild.initialize({
+      wasmURL: "https://deno.land/x/esbuild@v0.15.6/esbuild.wasm",
+      worker: false,
+    });
+    esbuildInit = true;
   }
 
   const result = await esbuild.build({
