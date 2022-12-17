2257b667802aa5158defc2254962c879ffebcbdc
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-12-17 22:42:01 +0000

    chore: rename functions

--- src/cargo/mod.ts
+++ src/cargo/mod.ts
@@ -4,5 +4,5 @@ export { title } from "./title.ts";
 export { head } from "./head.ts";
 export { body } from "./body.ts";
 export { html } from "./html.ts";
-export { page } from "./page.ts";
+export { pageFrom } from "./page.ts";
 export { findIslands, type Island } from "./islands.ts";
