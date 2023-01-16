9f90f95f37aac9f2f9a0adcf8a9614443ea03b1b
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-01-16 15:54:16 +0000

    chore: reduce esbuild deno plugin to last working version

--- src/cargo/deps.ts
+++ src/cargo/deps.ts
@@ -16,7 +16,7 @@ export {
 } from "../mod.ts";
 import * as esbuildNative from "https://deno.land/x/esbuild@v0.17.0/mod.js";
 import * as esbuildWasm from "https://deno.land/x/esbuild@v0.17.0/wasm.js";
-export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.6.0/mod.ts";
+export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.5.2/mod.ts";
 
 const esbuild = Deno.run === undefined ? esbuildWasm : esbuildNative;
 
