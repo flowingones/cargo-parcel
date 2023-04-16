08a3f068a7f5bd03d9a86d7c78d29a2bdffbb6b1
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-04-16 22:17:43 +0200

    feat: add status code to mapped path

--- src/cargo/path-mapping.test.ts
+++ src/cargo/path-mapping.test.ts
@@ -3,18 +3,19 @@ import { mappedPath } from "./path-mapping.ts";
 
 Deno.test(mappedPath.name, async (t) => {
   await t.step('Should map "/!404"', () => {
-    assertEquals(mappedPath("!404"), "/*");
-    assertEquals(mappedPath("/dir/!404"), "/dir/*");
-    assertEquals(mappedPath("/dir/dir/!404"), "/dir/dir/*");
+    assertEquals(mappedPath("!404").path, "/*");
+    assertEquals(mappedPath("/dir/!404").path, "/dir/*");
+    assertEquals(mappedPath("/dir/dir/!404").path, "/dir/dir/*");
   });
   await t.step("should map static routes", () => {
-    assertEquals(mappedPath("/file"), "/file");
-    assertEquals(mappedPath("/dir/file"), "/dir/file");
-    assertEquals(mappedPath("/dir/dir/file"), "/dir/dir/file");
+    assertEquals(mappedPath("/file").path, "/file");
+    assertEquals(mappedPath("/dir/file").path, "/dir/file");
+    assertEquals(mappedPath("/dir/dir/file").path, "/dir/dir/file");
   });
   await t.step("should map dynamic routes", () => {
-    assertEquals(mappedPath("/[file]"), "/:file");
-    assertEquals(mappedPath("/[dir]/[file]"), "/:dir/:file");
-    assertEquals(mappedPath("/[dir]/[dir]/[file]"), "/:dir/:dir/:file");
+    assertEquals(mappedPath("/[file]").path, "/:file");
+    assertEquals(mappedPath("/[dir]/[file]").path, "/:dir/:file");
+    assertEquals(mappedPath("/[dir]/[dir]/[file]").path, "/:dir/:dir/:file");
+    assertEquals(mappedPath("/[file]/!404").path, "/:file/*");
   });
 });
--- src/cargo/path-mapping.ts
+++ src/cargo/path-mapping.ts
@@ -2,17 +2,22 @@ import { parse } from "std/path/mod.ts";
 
 interface PathMapping {
   path: string;
+  statusCode: number;
 }
 
 const pathMappings = new Map<string, PathMapping>([
-  ["/!404", { path: "/*" }],
+  ["/!404", { path: "/*", statusCode: 404 }],
 ]);
 
-export function mappedPath(path: string): string {
+export function mappedPath(path: string): PathMapping {
   const parsedPath = parse(path);
   const mappedPath = pathMappings.get(`/${parsedPath.name}`);
+
+  let statusCode = 200;
+
   if (mappedPath) {
-    return `${parsedPath.dir}${
+    statusCode = mappedPath.statusCode;
+    path = `${parsedPath.dir}${
       parsedPath.dir === "/"
         ? mappedPath.path.replace(/\//, "")
         : mappedPath.path
@@ -23,5 +28,5 @@ export function mappedPath(path: string): string {
    * Prepare dynamic path segments for URLPattern
    * https://developer.mozilla.org/en-US/docs/Web/API/URLPattern
    */
-  return path.replace(/(\[(.+?)\])/g, ":$2");
+  return { path: path.replace(/(\[(.+?)\])/g, ":$2"), statusCode };
 }
