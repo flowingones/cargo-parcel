5beb85cf1837a68922b20d6e89758aad0bf52f47
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-09-03 21:33:24 +0200

    refactor: cleanup bundler changes

--- src/cargo/deps.ts
+++ src/cargo/deps.ts
@@ -7,10 +7,3 @@ export {
   type VNode,
   VType,
 } from "../mod.ts";
-import * as esbuildNative from "https://deno.land/x/esbuild@v0.17.0/mod.js";
-import * as esbuildWasm from "https://deno.land/x/esbuild@v0.17.0/wasm.js";
-export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.5.2/mod.ts";
-
-const esbuild = Deno.run === undefined ? esbuildWasm : esbuildNative;
-
-export { esbuild };
