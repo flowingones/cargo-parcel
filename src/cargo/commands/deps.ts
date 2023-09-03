156cf3d40494ffc8c118e06d1be71f13b73d65cd
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-09-03 21:32:58 +0200

    reafactor: remove load commands

--- src/cargo/commands/deps.ts
+++
@@ -1,7 +0,0 @@
-export {
-  dirname,
-  join,
-  parse,
-} from "https://deno.land/std@0.180.0/path/mod.ts";
-export { walk } from "https://deno.land/std@0.180.0/fs/mod.ts";
-export { parse as parseArgs } from "https://deno.land/std@0.180.0/flags/mod.ts";
