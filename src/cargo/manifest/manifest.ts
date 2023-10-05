e373a24480af157fd6de13c7f4857023b2c4ee0d
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-10-05 19:20:11 +0200

    fix: delete the manifest dir on every manifest  creation cycle

--- src/cargo/manifest/manifest.ts
+++ src/cargo/manifest/manifest.ts
@@ -1,6 +1,7 @@
 export async function createManifestDirectory() {
   try {
-    await Deno.stat(".manifest");
+    await Deno.remove(".manifest", { recursive: true });
+    await Deno.mkdir(".manifest");
   } catch (e) {
     if (e instanceof Deno.errors.NotFound) {
       await Deno.mkdir(".manifest");
